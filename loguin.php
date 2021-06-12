10373a596dc0526dc1fa9005e17725c360509492
cesar-cast <cesar.chachagua20@itca.edu.sv>
2021-06-12 14:28:15 -0600

    programe la interfas del inicio de secion

--- loguin.php
+++ loguin.php
@@ -1,14 +1,38 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Loguin</title>
+	<title>Login V13</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
 </head>
 <body style="background-color: #999999;" style="height: 80%;" >
-	<!-- Eduardo Miguel Castaneda  -->
-<div class="limiter">
+	
+	<div class="limiter">
 		<div class="container-login100">
 			<div class="login100-more" style="background-image: url('images/bg-01.jpg');"></div>
 
@@ -47,6 +71,52 @@
 			</div>
 		</div>
 	</div>
-	<!-- Final de Eduardo Miguel Castaneda -->
+	<?php
+   if(isset($_POST["enviarlog"]))
+   {
+      
+
+	   if(mysqli_num_rows( $G ) > 0)
+	   {
+        $tip=$G->fetch_assoc();
+           if($tip["tipo"]=="doctor")
+           {
+		   echo "<META HTTP-EQUIV='REFRESH' CONTENT='2;URL=doctor.php'>";
+           }
+          else if($tip["tipo"]=="secretaria")
+           {
+		   echo "<META HTTP-EQUIV='REFRESH' CONTENT='2;URL=doctor.php'>";
+           }
+          else if($tip["tipo"]=="cliente")
+           {
+		   echo "<META HTTP-EQUIV='REFRESH' CONTENT='2;URL=doctor.php'>";
+           }
+	   }
+       else
+       {
+           echo "usuario no exite";
+       }
+   }
+
+
+  ?>
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
 </body>
 </html>
