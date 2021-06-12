82f5ed5b13003b4cf1e048306453fc7831716b41
edurdo-mig <eduardo.castaneda20@itca.edu.sv>
2021-06-12 14:12:37 -0600

    Cree el código para recibir los comentarios de los pacientes

---
+++ procesoPHP/Envio-de-comentarios.php
@@ -0,0 +1,17 @@
+<?php
+
+if (isset($_POST['enviar'])) {
+    if (!empty($_POST['name']) && !empty($_POST['asunto']) && !empty($_POST['msg']) && !empty($_POST['email'])) {
+$name = $_POST['name'];
+$asunto = $_POST['asunto'];
+$msg = $_POST['msg'];
+$email = $_POST['email'];
+$header = "Frm: clinicasalud360@outlook.com" . "\r\n";
+$header.= "Reply-To: clinicasalud360@outlook.com" . "\r\n";
+$header.= "X-Mailer: PHP/". php version();
+$mail = @mail($email,$asunto,$msg,$header);
+if ($mail) {
+    echo "<h4>!Mail enviado exitosamente!</h4>";
+       }
+    }
+}
