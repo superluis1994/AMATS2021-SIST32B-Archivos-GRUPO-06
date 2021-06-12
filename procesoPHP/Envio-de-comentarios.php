3adfdbf811859e0ba60e946ecd1504bac2180e02
superluis1994 <alberto.sorto20@itca.edu.sv>
2021-06-12 16:41:14 -0600

    corregi una linea de codigo

--- procesoPHP/Envio-de-comentarios.php
+++ procesoPHP/Envio-de-comentarios.php
@@ -8,7 +8,7 @@ $msg = $_POST['msg'];
 $email = $_POST['email'];
 $header = "Frm: clinicasalud360@outlook.com" . "\r\n";
 $header.= "Reply-To: clinicasalud360@outlook.com" . "\r\n";
-$header.= "X-Mailer: PHP/". php version();
+$header.= "X-Mailer: PHP/". phpversion();
 $mail = @mail($email,$asunto,$msg,$header);
 if ($mail) {
     echo "<h4>!Mail enviado exitosamente!</h4>";
