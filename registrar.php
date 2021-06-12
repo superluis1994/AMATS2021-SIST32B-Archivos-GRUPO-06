f9006bfd4e483b6557a6b5401602efd6a2b5b2c2
edurdo-mig <eduardo.castaneda20@itca.edu.sv>
2021-06-12 12:41:26 -0600

    Cree la base para el registro de los pacientes

---
+++ registrar.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>REGISTRO</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<body style="background-color: #999999;" style="height: 80%;" >
+	
+</body>
+</html>
