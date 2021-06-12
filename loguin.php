4cfb1eda2780b4f28770f642c137f387d0f8a317
edurdo-mig <eduardo.castaneda20@itca.edu.sv>
2021-06-12 14:11:58 -0600

    Agrege el formulario

--- loguin.php
+++ loguin.php
@@ -7,7 +7,46 @@
 
 </head>
 <body style="background-color: #999999;" style="height: 80%;" >
-	
-	
+	<!-- Eduardo Miguel Castaneda  -->
+<div class="limiter">
+		<div class="container-login100">
+			<div class="login100-more" style="background-image: url('images/bg-01.jpg');"></div>
+
+			<div class="wrap-login100 p-l-50 p-r-50 p-t-72 p-b-50">
+				<form method="POST" class="login100-form validate-form">
+					<span class="login100-form-title p-b-59">
+						Sign Up
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<span class="label-input100">Correo</span>
+						<input class="input100" type="text" name="correo" placeholder="Email addess...">
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<span class="label-input100">Password</span>
+						<input class="input100" type="text" name="pass" placeholder="*************">
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="container-login100-form-btn">
+						<div class="wrap-login100-form-btn">
+							<div class="login100-form-bgbtn"></div>
+							<button type="submit" name="enviarlog" class="login100-form-btn">
+								Sign Up
+							</button>
+						</div>
+
+						<a href="#" class="dis-block txt3 hov1 p-r-30 p-t-10 p-b-10 p-l-30">
+							Sign in
+							<i class="fa fa-long-arrow-right m-l-5"></i>
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	<!-- Final de Eduardo Miguel Castaneda -->
 </body>
 </html>
