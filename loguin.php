35fe21245e9dfd1dc3104120779c78f43d8eab18
Kenneth-cell <kenneth.gonzalez20@itca.edu.sv>
2021-06-13 02:02:58 -0500

    Cree el archivo php para login

---
+++ loguin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Loguin</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<body style="background-color: #999999;" style="height: 80%;" >
+	
+	
+</body>
+</html>
